f29771bf14e56367a040e8b22393ec46c4a77be2
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-11-14 11:49:00 -0600

    Fix: corrigiendo el scroll de las peticiones

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,9 +25,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   }
 
   Future<void> logout() async {
-    // Aquí puedes añadir lógica de cierre de sesión si es necesario
-    Navigator.pushReplacementNamed(
-        context, '/'); // Redirige a la pantalla de login
+    Navigator.pushReplacementNamed(context, '/');
   }
 
   @override
@@ -52,198 +50,191 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Espacio y texto "CONEXIÓN" y "FITEC"
-            const SizedBox(height: 40), // Espacio desde la parte superior
-            const Text(
-              'CONEXIÓN',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFFFFC107), // Color amarillo
-              ),
+      body: Column(
+        children: [
+          const SizedBox(height: 40),
+          const Text(
+            'CONEXIÓN',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFFFC107),
             ),
-            const Text(
-              'FITEC',
-              style: TextStyle(
-                fontSize: 16,
-                letterSpacing: 2,
-                color: Colors.grey,
-              ),
+          ),
+          const Text(
+            'FITEC',
+            style: TextStyle(
+              fontSize: 16,
+              letterSpacing: 2,
+              color: Colors.grey,
             ),
-            const SizedBox(
-                height: 20), // Espacio adicional entre el texto y las pestañas
+          ),
+          const SizedBox(height: 20),
 
-            // TabBarView con las peticiones
-            SizedBox(
-              height:
-                  MediaQuery.of(context).size.height - 180, // Ajuste la altura
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  // Pestaña 1: Todas las Peticiones
-                  FutureBuilder(
-                    future: getPeticiones(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return ListView.builder(
-                          itemCount: snapshot.data?.length,
-                          itemBuilder: (context, index) {
-                            final peticionData = snapshot.data?[index];
-                            final fechaString = peticionData['fecha'];
-                            final peticion = peticionData['peticion'];
-                            final cumplida = peticionData['cumplida'];
+          // TabBarView con scroll
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                // Pestaña 1: Todas las Peticiones
+                FutureBuilder(
+                  future: getPeticiones(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, index) {
+                          final peticionData = snapshot.data?[index];
+                          final fechaString = peticionData['fecha'];
+                          final peticion = peticionData['peticion'];
+                          final cumplida = peticionData['cumplida'];
 
-                            DateTime fecha;
-                            try {
-                              fecha = DateTime.parse(fechaString);
-                            } catch (e) {
-                              fecha = DateTime.now();
-                              print("Error al convertir la fecha: $e");
-                            }
+                          DateTime fecha;
+                          try {
+                            fecha = DateTime.parse(fechaString);
+                          } catch (e) {
+                            fecha = DateTime.now();
+                            print("Error al convertir la fecha: $e");
+                          }
 
-                            final fechaFormateada =
-                                DateFormat('dd/MM/yyyy').format(fecha);
+                          final fechaFormateada =
+                              DateFormat('dd/MM/yyyy').format(fecha);
 
-                            return Card(
-                              margin: const EdgeInsets.symmetric(
-                                  vertical: 8.0, horizontal: 16.0),
-                              elevation: 4,
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'Fecha: $fechaFormateada',
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w500,
-                                            color: Colors.grey,
-                                          ),
+                          return Card(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 16.0),
+                            elevation: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Fecha: $fechaFormateada',
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.grey,
                                         ),
-                                        Icon(
-                                          cumplida
-                                              ? Icons.check_circle
-                                              : Icons.cancel,
-                                          color: cumplida
-                                              ? Colors.green
-                                              : Colors.red,
-                                          size: 20,
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                      peticion,
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  ),
-
-                  // Pestaña 2: Mis Peticiones
-                  FutureBuilder(
-                    future: getPeticionesByUserId(userId),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return ListView.builder(
-                          itemCount: snapshot.data?.length,
-                          itemBuilder: (context, index) {
-                            final peticionData = snapshot.data?[index];
-                            final fechaString = peticionData['fecha'];
-                            final peticion = peticionData['peticion'];
-                            final cumplida = peticionData['cumplida'];
-
-                            DateTime fecha;
-                            try {
-                              fecha = DateTime.parse(fechaString);
-                            } catch (e) {
-                              fecha = DateTime.now();
-                              print("Error al convertir la fecha: $e");
-                            }
-
-                            final fechaFormateada =
-                                DateFormat('dd/MM/yyyy').format(fecha);
-
-                            return Card(
-                              margin: const EdgeInsets.symmetric(
-                                  vertical: 8.0, horizontal: 16.0),
-                              elevation: 4,
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'Fecha: $fechaFormateada',
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w500,
-                                            color: Colors.grey,
-                                          ),
-                                        ),
-                                        Icon(
-                                          cumplida
-                                              ? Icons.check_circle
-                                              : Icons.cancel,
-                                          color: cumplida
-                                              ? Colors.green
-                                              : Colors.red,
-                                          size: 20,
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                      peticion,
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
+                                      Icon(
+                                        cumplida
+                                            ? Icons.check_circle
+                                            : Icons.cancel,
+                                        color: cumplida
+                                            ? Colors.green
+                                            : Colors.red,
+                                        size: 20,
                                       ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    peticion,
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
                                     ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
-                            );
-                          },
-                        );
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  ),
-                ],
-              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
+
+                // Pestaña 2: Mis Peticiones
+                FutureBuilder(
+                  future: getPeticionesByUserId(userId),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, index) {
+                          final peticionData = snapshot.data?[index];
+                          final fechaString = peticionData['fecha'];
+                          final peticion = peticionData['peticion'];
+                          final cumplida = peticionData['cumplida'];
+
+                          DateTime fecha;
+                          try {
+                            fecha = DateTime.parse(fechaString);
+                          } catch (e) {
+                            fecha = DateTime.now();
+                            print("Error al convertir la fecha: $e");
+                          }
+
+                          final fechaFormateada =
+                              DateFormat('dd/MM/yyyy').format(fecha);
+
+                          return Card(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 16.0),
+                            elevation: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Fecha: $fechaFormateada',
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                      Icon(
+                                        cumplida
+                                            ? Icons.check_circle
+                                            : Icons.cancel,
+                                        color: cumplida
+                                            ? Colors.green
+                                            : Colors.red,
+                                        size: 20,
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    peticion,
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
